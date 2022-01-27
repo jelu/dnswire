dcd41ec156df5658aadee7980ed8a57d81020726
Jerry Lundström <lundstrom.jerry@gmail.com>
2022-01-27 11:02:51 +0100

    .

--- examples/client_receiver_uv.c
+++ examples/client_receiver_uv.c
@@ -1,4 +1,5 @@
 #include <dnswire/reader.h>
+#include <dnswire/writer.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,6 +20,9 @@ uv_connect_t conn;
 char         rbuf[BUF_SIZE];
 
 struct dnswire_reader reader;
+
+FILE* fp = 0;
+struct dnswire_writer writer;
 
 void client_alloc_buffer(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf)
 {
@@ -45,6 +49,26 @@ void client_read(uv_stream_t* handle, ssize_t nread, const uv_buf_t* buf)
                  * We got a DNSTAP message, lets print it!
                  */
                 print_dnstap(dnswire_reader_dnstap(reader));
+                if (fp) {
+                    dnswire_writer_set_dnstap(writer, dnswire_reader_dnstap(reader));
+                    int done = 0;
+                    while (!done) {
+                        switch (dnswire_writer_fwrite(&writer, fp)) {
+                        case dnswire_ok:
+                            done = 1;
+                            break;
+                        case dnswire_again:
+                            break;
+                        case dnswire_endofdata:
+                            done = 1;
+                            break;
+                        default:
+                            fprintf(stderr, "dnswire_writer_fwrite() error\n");
+                            done = 1;
+                        }
+                    }
+                    fflush(fp);
+                }
                 break;
             case dnswire_again:
             case dnswire_need_more:
@@ -87,11 +111,25 @@ void on_connect(uv_connect_t* req, int status)
     uv_read_start((uv_stream_t*)&sock, client_alloc_buffer, client_read);
 }
 
+
 int main(int argc, const char* argv[])
 {
     if (argc < 3) {
-        fprintf(stderr, "usage: client_receiver_uv <IP> <port>\n");
+        fprintf(stderr, "usage: client_receiver_uv <IP> <port> [file]\n");
         return 1;
+    }
+
+    if (argc == 4) {
+        fp = fopen(argv[3], "w");
+        if (!fp) {
+            fprintf(stderr, "Unable to open %s: %s\n", argv[3], strerror(errno));
+            return 1;
+        }
+
+        if (dnswire_writer_init(&writer) != dnswire_ok) {
+            fprintf(stderr, "Unable to initialize dnswire writer\n");
+            return 1;
+        }
     }
 
     /*
